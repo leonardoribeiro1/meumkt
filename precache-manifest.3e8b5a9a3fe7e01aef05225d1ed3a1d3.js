a3fafdaf54986661168e417819e35cb10cb82ec2
Leonardo Ribeiro <leonardoribeiro@MacBook-Air-de-Leonardo.local>
2020-05-21 19:06:38 -0300

    Updates

---
+++ precache-manifest.3e8b5a9a3fe7e01aef05225d1ed3a1d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beed0b19f0c49f28f7f9bfeea2245b07",
+    "url": "/meumkt/index.html"
+  },
+  {
+    "revision": "738f04374872bbca4040",
+    "url": "/meumkt/static/css/main.5971d584.chunk.css"
+  },
+  {
+    "revision": "6d78434ea9f077d94e0b",
+    "url": "/meumkt/static/js/2.bdccdb1c.chunk.js"
+  },
+  {
+    "revision": "738f04374872bbca4040",
+    "url": "/meumkt/static/js/main.dbf6ea63.chunk.js"
+  },
+  {
+    "revision": "f11149cd124798d42e9e",
+    "url": "/meumkt/static/js/runtime~main.57b5f2c1.js"
+  },
+  {
+    "revision": "d3d01568c352a21dc221577001dce801",
+    "url": "/meumkt/static/media/AVISO.d3d01568.png"
+  },
+  {
+    "revision": "cb71b9ca2d56a5f807c044d1a623220a",
+    "url": "/meumkt/static/media/BANNER.cb71b9ca.png"
+  },
+  {
+    "revision": "db0654541bf6d6e8dd0033b7e3322a3e",
+    "url": "/meumkt/static/media/LOGO-MEUHALL-SF.db065454.png"
+  },
+  {
+    "revision": "5253569598326cb664c557b756312929",
+    "url": "/meumkt/static/media/MENSALIDADE.52535695.png"
+  },
+  {
+    "revision": "a711bd32e8659d46a190117d0c1dd990",
+    "url": "/meumkt/static/media/VISITANTES.a711bd32.png"
+  },
+  {
+    "revision": "27d5a3478ee87945ed55f988c5447cc0",
+    "url": "/meumkt/static/media/aluno.27d5a347.png"
+  },
+  {
+    "revision": "d62fe09cb6e942ff0f9d066b2bcf3396",
+    "url": "/meumkt/static/media/bg102.d62fe09c.jpg"
+  },
+  {
+    "revision": "bfbe8bf69e3b3cb87780736c21f8fcb5",
+    "url": "/meumkt/static/media/bg105.bfbe8bf6.jpg"
+  },
+  {
+    "revision": "e4ce5ef9e0f39f6372cdbc9f53fc2674",
+    "url": "/meumkt/static/media/bg106.e4ce5ef9.jpg"
+  },
+  {
+    "revision": "b2c0ec42a3fb99836b80dd0002423f11",
+    "url": "/meumkt/static/media/bg107.b2c0ec42.jpg"
+  },
+  {
+    "revision": "e81445dea015adc51acb7e0525e64c9d",
+    "url": "/meumkt/static/media/bg112.e81445de.jpg"
+  },
+  {
+    "revision": "9e2ac66f7531677700addb1162cae5bf",
+    "url": "/meumkt/static/media/cliente.9e2ac66f.jpg"
+  },
+  {
+    "revision": "4fe042eeeeb75df95e602141c9028968",
+    "url": "/meumkt/static/media/logo.4fe042ee.png"
+  },
+  {
+    "revision": "730386300b28bd974f38306ddbc0f778",
+    "url": "/meumkt/static/media/logo12.73038630.png"
+  },
+  {
+    "revision": "fc285ad4dd63ced1f99697b26ef85b9c",
+    "url": "/meumkt/static/media/logo2.fc285ad4.png"
+  },
+  {
+    "revision": "3b175c9cfffbcc5b4f67048b0093e1dd",
+    "url": "/meumkt/static/media/school.3b175c9c.png"
+  }
+]);
